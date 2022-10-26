c1475007bfe49d9018e641220e5b185161a1ad49
Gabby <giobagiu@gmail.com>
2022-10-25 19:15:42 -0600

    Mandelbrot Set with corresponding Julia Set

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MandelbrotTry
 {
-    partial class Form1
+    partial class MandelbrotSet
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,21 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MandelbrotPanel = new System.Windows.Forms.Panel();
+            this.JuliaSetPanel = new System.Windows.Forms.Panel();
+            this.MouseLabelX = new System.Windows.Forms.Label();
+            this.MouseLabelY = new System.Windows.Forms.Label();
+            this.TestLbl = new System.Windows.Forms.Label();
+            this.MandelbrotBtn = new System.Windows.Forms.Button();
+            this.JuliaSetBtn = new System.Windows.Forms.Button();
+            this.Test2Lbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // MandelbrotPanel
+            // 
+            this.MandelbrotPanel.Location = new System.Drawing.Point(12, 12);
+            this.MandelbrotPanel.Name = "MandelbrotPanel";
+            this.MandelbrotPanel.Size = new System.Drawing.Size(500, 500);
+            this.MandelbrotPanel.TabIndex = 0;
+            this.MandelbrotPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MandelbrotPanelPaint);
+            this.MandelbrotPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MandelbrotPanel_MouseDown);
+            // 
+            // JuliaSetPanel
+            // 
+            this.JuliaSetPanel.Location = new System.Drawing.Point(672, 12);
+            this.JuliaSetPanel.Name = "JuliaSetPanel";
+            this.JuliaSetPanel.Size = new System.Drawing.Size(500, 500);
+            this.JuliaSetPanel.TabIndex = 1;
+            this.JuliaSetPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.JuliaSetPanel_Paint);
+            // 
+            // MouseLabelX
+            // 
+            this.MouseLabelX.AutoSize = true;
+            this.MouseLabelX.Location = new System.Drawing.Point(678, 525);
+            this.MouseLabelX.Name = "MouseLabelX";
+            this.MouseLabelX.Size = new System.Drawing.Size(102, 15);
+            this.MouseLabelX.TabIndex = 2;
+            this.MouseLabelX.Text = "Mouse X Position:";
+            // 
+            // MouseLabelY
+            // 
+            this.MouseLabelY.AutoSize = true;
+            this.MouseLabelY.Location = new System.Drawing.Point(874, 525);
+            this.MouseLabelY.Name = "MouseLabelY";
+            this.MouseLabelY.Size = new System.Drawing.Size(102, 15);
+            this.MouseLabelY.TabIndex = 3;
+            this.MouseLabelY.Text = "Mouse Y Position:";
+            // 
+            // TestLbl
+            // 
+            this.TestLbl.AutoSize = true;
+            this.TestLbl.Location = new System.Drawing.Point(213, 537);
+            this.TestLbl.Name = "TestLbl";
+            this.TestLbl.Size = new System.Drawing.Size(0, 15);
+            this.TestLbl.TabIndex = 4;
+            // 
+            // MandelbrotBtn
+            // 
+            this.MandelbrotBtn.Location = new System.Drawing.Point(538, 119);
+            this.MandelbrotBtn.Name = "MandelbrotBtn";
+            this.MandelbrotBtn.Size = new System.Drawing.Size(102, 47);
+            this.MandelbrotBtn.TabIndex = 5;
+            this.MandelbrotBtn.Text = "Generate Mandelbrot Set";
+            this.MandelbrotBtn.UseVisualStyleBackColor = true;
+            this.MandelbrotBtn.Click += new System.EventHandler(this.MandelbrotBtn_Click);
+            // 
+            // JuliaSetBtn
+            // 
+            this.JuliaSetBtn.Location = new System.Drawing.Point(548, 263);
+            this.JuliaSetBtn.Name = "JuliaSetBtn";
+            this.JuliaSetBtn.Size = new System.Drawing.Size(81, 43);
+            this.JuliaSetBtn.TabIndex = 6;
+            this.JuliaSetBtn.Text = "Generate Julia Set";
+            this.JuliaSetBtn.UseVisualStyleBackColor = true;
+            this.JuliaSetBtn.Click += new System.EventHandler(this.JuliaSetBtn_Click);
+            // 
+            // Test2Lbl
+            // 
+            this.Test2Lbl.AutoSize = true;
+            this.Test2Lbl.Location = new System.Drawing.Point(286, 537);
+            this.Test2Lbl.Name = "Test2Lbl";
+            this.Test2Lbl.Size = new System.Drawing.Size(0, 15);
+            this.Test2Lbl.TabIndex = 7;
+            // 
+            // MandelbrotSet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.Test2Lbl);
+            this.Controls.Add(this.JuliaSetBtn);
+            this.Controls.Add(this.MandelbrotBtn);
+            this.Controls.Add(this.TestLbl);
+            this.Controls.Add(this.MouseLabelY);
+            this.Controls.Add(this.MouseLabelX);
+            this.Controls.Add(this.JuliaSetPanel);
+            this.Controls.Add(this.MandelbrotPanel);
+            this.Name = "MandelbrotSet";
+            this.Text = "Mandelbrot Set";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Panel MandelbrotPanel;
+        private Panel JuliaSetPanel;
+        private Label MouseLabelX;
+        private Label MouseLabelY;
+        private Label TestLbl;
+        private Button MandelbrotBtn;
+        private Button JuliaSetBtn;
+        private Label Test2Lbl;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,75 +1,233 @@
 using System.Runtime.CompilerServices;
+using System.Drawing.Imaging;
 
 namespace MandelbrotTry
 {
-    public partial class Form1 : Form
+    public partial class MandelbrotSet : Form
     {
-
-        float graphWidth;
-        float windowHeight;
+        // global variables declared here. This is how zoomed in the mandelbrot set will be.
+        float scale = 2f;
         
+        // This bool stops the panels from drawing the set upon the window opening for the first time.
+        // prevents lag
+        bool formStarted = false;
+
+        // These are used for the mandelbrot and julia sets
+        float xMin;
+        float xMax;
+        float yMin;
+        float yMax;
+
+        float xDisplacement;
+        float yDisplacement;
+
+        // These are used to choose a julia set
+        float mouseX;
+        float mouseY;
+
+        float juliaWidth;
+        float juliaHeight;
+        float mandelWidth;
+        float mandelHeight;
 
 
-        public Form1()
+
+        public MandelbrotSet()
         {
             InitializeComponent();
         }
 
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        // This paints the mandelbrot set inside the left panel.
+        private void MandelbrotPanelPaint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            Brush brush;
-
-            graphWidth = 3;
-            windowHeight = (graphWidth * Size.Height) / Size.Width;
-
-            float xMin = -(graphWidth / 2);
-            float yMin = -(windowHeight / 2);
-            float xMax = xMin + graphWidth;
-            float yMax = yMin + windowHeight;
-
-            int maxIterations = 100;
-
-            float dX = (xMax - xMin) / Size.Width;
-            float dY = (yMax - yMin) / Size.Height;
-
-            float y = yMin;
-
-            for (int i = 0; i < Size.Height; i++)
+            // Checks if button has been pressed. If not, don't paint. Prevents lag upon window loading for the first time.
+            if (formStarted == true)
             {
-                float x = xMin;
-                for (int j = 0; j < Size.Width; j++)
-                {
-                    float a = x;
-                    float b = y;
-                    int n = 0;
-                    while (n < maxIterations)
-                    {
-                        float aa = a * a;
-                        float bb = b * b;
-                        float twoAB = 2f * a * b;
-                        a = aa - bb + x;
-                        b = twoAB + y;
-                        if((aa * aa) + (bb * bb) > 16f)
-                        {
-                            break;
-                        }
-                        n++;
-                    }
+                // Stuff is declared for drawing.
+                Graphics g = e.Graphics;
+                Brush brush;
 
-                    if (n == maxIterations)
+                // variables for height and width of the panel.
+                float height = MandelbrotPanel.Height;
+                float width = MandelbrotPanel.Width;
+
+                // Crazy math is done.
+                // Width is declared as a value and height is declared so as to keep the ratio between height and width constant.
+                mandelWidth = scale;
+                mandelHeight = (mandelWidth * height) / width;
+
+                // This just moves the set around a bit.
+                xDisplacement = -0.5f;
+                yDisplacement = 0f;
+
+                // Upper and lower bounds are declared.
+                xMin = -(mandelWidth / 2) + xDisplacement;
+                yMin = -(mandelHeight / 2) + yDisplacement;
+                xMax = (mandelWidth / 2) + xDisplacement;
+                yMax = (mandelHeight / 2) + yDisplacement;
+
+                // The max amount of times the recursion below will repeat for a single pixel.
+                float maxIterations = 100;
+
+                // Change in X and Y so as to move exactly 1 pixel forward.
+                float dX = (xMax - xMin) / height;
+                float dY = (yMax - yMin) / width;
+
+                // Generation starts here. X begins at the lower bound.
+                float x = xMin;
+                for (int i = 0; i < height; i++)
+                {
+                    // Y begins at the lower bound and each time the for loop above loops,
+                    float y = yMin;
+                    for (int j = 0; j < width; j++)
                     {
-                        brush = new SolidBrush(Color.FromArgb(255,0,0,0));
+                        float a = x;
+                        float b = y;
+                        int n = 0;
+                        while (n < maxIterations)
+                        {
+                            n++;
+                            float aa = a * a;
+                            float bb = b * b;
+                            float twoAB = 2f * a * b;
+                            a = aa - bb + x;
+                            b = twoAB + y;
+                            if ((aa * aa) + (bb * bb) > 100)
+                            {
+                                break;
+                            }
+
+                        }
+
+                        if (n == maxIterations)
+                        {
+                            brush = new SolidBrush(Color.FromArgb(255, 0, 0, 0));
+                        }
+                        else
+                        {
+
+                            float bright = Lerp(0, 1, MathF.Pow(n/maxIterations, 1.0f/2.0f));
+                            bright = MathF.Round(bright * 255);
+                            brush = new SolidBrush(Color.FromArgb(255, 
+                                (int)(Lerp(0, 1, MathF.Pow(n / maxIterations, 1.0f / 2.0f)) * 255), 
+                                (int)(Lerp(0, 1, MathF.Pow(n / maxIterations, 1.0f / 5.0f)) * 255),
+                                (int)(Lerp(0, 1, MathF.Pow(n / maxIterations, 1.0f / 1.0f)) * 255)));
+                        }
+
+                        g.FillRectangle(brush, i, j, 500, 1);
+                        y += dY;
                     }
-                    else
-                    {
-                        brush = new SolidBrush(Color.FromArgb(255, (int)Math.Round(MathF.Sqrt(n/maxIterations)), 255, 255));
-                    }
-                    g.FillRectangle(brush, i, j, 1, 1);
                     x += dX;
                 }
-                y += dY;
             }
+            
+        }
+
+        float Lerp(float start, float end, float target)
+        {
+            // start, end, target is where between the two values I return
+            float output =  (1-target)*start + target*end;
+            return output;
+        }
+
+        private void MandelbrotBtn_Click(object sender, EventArgs e)
+        {
+            formStarted = true;
+            MandelbrotPanel.Invalidate();
+        }
+
+        private void MandelbrotPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            TestLbl.Text = e.Location.X + ":" + e.Location.Y;
+            float mouseAreaWidth = scale;
+            float mouseAreaHeight = (mouseAreaWidth * JuliaSetPanel.Height) / JuliaSetPanel.Width;
+            
+
+            mouseX = (e.Location.X / (float)MandelbrotPanel.Width) * scale - 1.5f;
+            mouseY = (e.Location.Y / (float)MandelbrotPanel.Height) * -scale +1;
+
+            Test2Lbl.Text = mouseX + ":" + mouseY;
+
+        }
+
+        private void JuliaSetPanel_Paint(object sender, PaintEventArgs e)
+        {
+            if (formStarted == true)
+            {
+                Graphics g = e.Graphics;
+                Brush brush;
+
+                float height = JuliaSetPanel.Height;
+                float width = JuliaSetPanel.Width;
+
+                juliaWidth = 3;
+                juliaHeight = (juliaWidth * height) / width;
+
+                xDisplacement = 0f;
+                yDisplacement = 0f;
+
+                float xMin = -juliaWidth / 2;
+                float yMin = -juliaHeight / 2;
+                float xMax = juliaWidth / 2;
+                float yMax = juliaHeight / 2;
+
+                float maxIterations = 20;
+
+                float dX = (xMax - xMin) / height;
+                float dY = (yMax - yMin) / width;
+
+                float x = xMin;
+
+                for (int i = 0; i < height; i++)
+                {
+                    float y = yMin;
+                    for (int j = 0; j < width; j++)
+                    {
+                        float a = x;
+                        float b = y;
+                        int n = 0;
+                        while (n < maxIterations)
+                        {
+                            n++;
+                            float aa = a * a;
+                            float bb = b * b;
+                            float twoAB = 2f * a * b;
+                            a = aa - bb + mouseX;
+                            b = twoAB + mouseY;
+                            if ((aa * aa) + (bb * bb) > 16)
+                            {
+                                break;
+                            }
+
+                        }
+
+                        if (n == maxIterations)
+                        {
+                            brush = new SolidBrush(Color.FromArgb(255, 0, 0, 0));
+                        }
+                        else
+                        {
+
+                            float bright = Lerp(0, 1, MathF.Pow(n / maxIterations, 1.0f / 2.0f));
+                            bright = MathF.Round(bright * 255);
+                            brush = new SolidBrush(Color.FromArgb(255,
+                                (int)(Lerp(0, 1, MathF.Pow(n / maxIterations, 1.0f / 2.0f)) * 255),
+                                (int)(Lerp(0, 1, MathF.Pow(n / maxIterations, 1.0f / 5.0f)) * 255),
+                                255));
+                        }
+
+                        g.FillRectangle(brush, i, j, 500, 1);
+                        y += dY;
+                    }
+                    x += dX;
+                }
+            }
+
+        }
+
+        private void JuliaSetBtn_Click(object sender, EventArgs e)
+        {
+            JuliaSetPanel.Invalidate();
         }
     }
 }
